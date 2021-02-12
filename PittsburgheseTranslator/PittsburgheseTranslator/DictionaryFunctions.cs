f0c308d707c1156afc3347a7dd9a9297f49b7fba
Charles Kaminski <cvk1997@gmail.com>
2021-02-11 22:03:45 -0500

    got the list working so far

--- PittsburgheseTranslator/PittsburgheseTranslator/DictionaryFunctions.cs
+++ PittsburgheseTranslator/PittsburgheseTranslator/DictionaryFunctions.cs
@@ -7,30 +7,43 @@ namespace PittsburgheseTranslator
 {
     public class DictionaryFunctions
     {
-        
-        private bool createDictionary()
-        {Dictionary<string, string> pgheseDictionary = new Dictionary<string, string>();
-            
+        public Dictionary<string, string> pgheseDictionary { get; set; } = new Dictionary<string, string>();
+        public Dictionary<string, string> createDictionary()
+        {
+
             string directory = Environment.CurrentDirectory;
             string fileName = "pittsburghese.txt";
-            string fullPath=Path.Combine(directory, fileName);
+            string fullPath = Path.Combine(directory, fileName);
             try
             {
                 using (StreamReader sr = new StreamReader(fullPath))
                 {
-                    string[] wordLine = sr.ReadLine().Split('|');
-                    pgheseDictionary[wordLine[0]] = wordLine[1];
+                    while (!sr.EndOfStream)
+                    {
+                        string[] wordLine = sr.ReadLine().Split('|');
+                        pgheseDictionary[wordLine[0]] = wordLine[1];
+                    }
                 }
-            }catch(FileNotFoundException e)
+            }
+            catch (FileNotFoundException e)
             {
                 Console.WriteLine("Something must have happened to the file.  Please contact support!");
-            }catch (Exception e)
+            }
+            catch (Exception e)
             {
                 Console.WriteLine("Please contact support");
             }
+            return pgheseDictionary;
+        }
+        public bool WordList()
+        {
+            foreach (KeyValuePair<string, string> kvp in pgheseDictionary)
+            {
+                Console.WriteLine(kvp.Key);
+            }
             return true;
+
         }
     }
-    
     
 }
--- PittsburgheseTranslator/PittsburgheseTranslator/UI.cs
+++ PittsburgheseTranslator/PittsburgheseTranslator/UI.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+ 
 namespace PittsburgheseTranslator
 {
     public class UI
     {
-        private DictionaryFunctions functions { get; set; } = new DictionaryFunctions();
+        private DictionaryFunctions Dictionary { get; set; } = new DictionaryFunctions();
+        public UI()
+        {
+            
+        }
         public void Start()
         {
             //DictionaryFunctions functions = new DictionaryFunctions();
+            Dictionary.createDictionary();
             bool searchAgain = true;
             Console.WriteLine("Welcome to the Pittsburghese Translator.  " +
                 "If you are ever in Pittsburgh and hear an odd sounding, slightly slurred word, " +
@@ -27,27 +32,32 @@ namespace PittsburgheseTranslator
         public string MainMenu()
         {
             string choice = "";
-            do
-            {
+            
+            
                 Console.WriteLine("Please select from the following options:\n" +
                     "1)Define a word\n" +
                     "2)See a List of Pittsburghese words\n" +
                     "3)Exit\n");
                 choice = Console.ReadLine();
-            } while (choice != "1" && choice != "2" && choice != "3");
+             while (choice != "1" && choice != "2" && choice != "3")
+            {
+                Console.WriteLine("Sorry, please choose 1, 2, or 3");
+                choice = Console.ReadLine();
+            }
             return choice;
                       
         }
         public bool MainMenuOptions(string choice)
         {
             bool searchAgain = true;
+            
             switch(choice)
             {
                 case "1":
                     WordToSearch();
                     break;
                 case "2":
-                    WordList();
+                    Dictionary.WordList();
                     break;
                 case "3":
                     searchAgain = false;
@@ -65,9 +75,7 @@ namespace PittsburgheseTranslator
             string inputWord = Console.ReadLine();
             return "";
         }
-        public bool WordList()
-        {
-            return true;
-        }
+       
+        
     }
 }
